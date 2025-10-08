06a5edf8c9f2a0106db3f93375f3d46e5a1d8b70
Jade2097 <jadedogo08@gmail.com>
2025-10-08 14:41:56 +0200

    display

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="app-container">
       {view === 'home' && <Home onEvaluate={handleEvaluation} />}
       {view === 'results' && <Results repoData={repoData} notes={notes} />}
     </div>
